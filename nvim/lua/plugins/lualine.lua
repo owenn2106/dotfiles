b4bf4a880845505c4734b5887d60d8c678fc5c32
Owenn Gimli <75826315+owenn2106@users.noreply.github.com>
2024-01-02 13:05:09 +0900

    fix: show relative path for files in lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -9,6 +9,9 @@ return {
         section_separators = "",
         component_separators = "",
       },
+      sections = {
+        lualine_c = { { "filename", path = 1 } },
+      },
     }
   end,
 }
